667457673d3d909b7fdd79c278e8fdb8a89e960b
BryanFinanci <bryanfinanci@gmail.com>
2026-01-10 02:03:57 -0300

    commit inicial

---
+++ src/main/java/com/bryangabriel/sistema_de_pedidos_e_pagamentos/SistemaDePedidosEPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.bryangabriel.sistema_de_pedidos_e_pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosEPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosEPagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryangabriel/sistema_de_pedidos_e_pagamentos/SistemaDePedidosEPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryangabriel.sistema_de_pedidos_e_pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidosEPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
