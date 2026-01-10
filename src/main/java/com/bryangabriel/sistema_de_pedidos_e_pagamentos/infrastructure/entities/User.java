903a4e03568944b23d0fef3959bd70c3436878e7
BryanFinanci <bryanfinanci@gmail.com>
2026-01-10 03:09:36 -0300

    feat: adiciona entidades de dominio e relacionamentos JPA

---
+++ src/main/java/com/bryangabriel/sistema_de_pedidos_e_pagamentos/infrastructure/entities/User.java
@@ -0,0 +1,6 @@
+package com.bryangabriel.sistema_de_pedidos_e_pagamentos.infrastructure.entities;
+
+ @author bryan
+ 
+public class User {
+}
---
+++ src/main/java/com/bryangabriel/sistema_de_pedidos_e_pagamentos/infrastructure/entities/enums/StatusDoPedido.java
@@ -0,0 +1,6 @@
+package com.bryangabriel.sistema_de_pedidos_e_pagamentos.infrastructure.entities.enums;
+
+ @author bryan
+ 
+public enum StatusDoPedido {
+}
